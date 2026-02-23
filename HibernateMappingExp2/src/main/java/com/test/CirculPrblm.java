88f0ae0aec08f04254e4ac92b2dc47e9a6e04607
rachana-jagannath <rachanajagannath@gmail.com>
2026-02-23 17:23:26 +0530

    Add files via upload

---
+++ HibernateMappingExp2/src/main/java/com/test/CirculPrblm.java
@@ -0,0 +1,20 @@
+package com.test;
+
+
+	class A
+	{
+		
+	}
+	class B
+	{
+		//B a=new B();
+	}
+	public class CirculPrblm{
+		public static void main(String[] args) {
+			System.out.println("main");
+			B b=new B();
+			System.out.println("after object");
+			
+		}
+	}
+
