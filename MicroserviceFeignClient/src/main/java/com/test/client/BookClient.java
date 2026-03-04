817a665742130749cef056c5f078ac7ee29a61a2
Rachana Jagannath <rachanajagannath@gmail.com>
2026-03-04 17:26:48 +0530

    Add files via upload

---
+++ MicroserviceFeignClient/src/main/java/com/test/client/BookClient.java
@@ -0,0 +1,21 @@
+package com.test.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.test.entity.Book;
+
+@FeignClient(
+		name="SpringBootRestAPI",
+		url="http://localhost:7799"
+		
+		)
+
+public interface BookClient {
+	@GetMapping("/list")
+	public List<Book>listAllBooks();
+	
+	
+}
---
+++ SpringBootCloudAPIGateway/src/main/java/com/test/config/CouldGatewayConfig.java
@@ -0,0 +1,16 @@
+package com.test.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CouldGatewayConfig {
+	@Bean
+protected RouteLocator getRouteConfig(RouteLocatorBuilder builder)
+{
+	return builder.routes().route(r->r.path("/product1/*").uri("http://localhost:8899")).route(r->r.path("/product2/*").uri("http://localhost:9988/*")).build();
+	
+}
+}
